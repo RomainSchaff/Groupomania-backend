9577a40be0fbd87b3ddf4afdaf66baeeab6d453c
Romain Schaff <romainschaff2612@gmail.com>
2023-03-16 02:53:03 +0100

    update origins

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const postRoutes = require("./routes/post");
 const commentRoutes = require("./routes/comment");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://romainschaff.github.io, http://localhost:3000/"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
