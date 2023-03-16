4084f44023cc686e08ae579fd52c814645c20235
Romain Schaff <romainschaff2612@gmail.com>
2023-03-16 02:10:17 +0100

    update origins/script start

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const postRoutes = require("./routes/post");
 const commentRoutes = require("./routes/comment");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
